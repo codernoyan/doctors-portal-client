4f0038bde2468012f7776c9a043ca2d66717568e
codernoyan <coder.noyan@gmail.com>
2022-11-12 00:05:01 +0600

    added project color configaration and added hero banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: '#0FCFEC',
+          secondary: '#19D3AE',
+          accent: "#3A4256",
+          neutral: "#292C3D",
+          "base-100": "#F1F2F3",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
