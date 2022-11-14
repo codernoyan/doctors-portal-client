22217135b46ab37e83ececc54c431a8adb57702e
codernoyan <coder.noyan@gmail.com>
2022-11-14 06:49:13 +0600

    login form with validation added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,22 +3,36 @@ import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
-  const [data, setData] = useState('');
+  const { register, formState: { errors }, handleSubmit } = useForm();
+
+  const handleLogin = (data) => {
+    console.log(data);
+  }
 
   return (
     <div className="h-[800px] flex justify-center items-center">
       <div className="w-96 p-7 shadow-md">
         <h2 className="text-xl">Login</h2>
-        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <form onSubmit={handleSubmit(handleLogin)}>
           <div className="form-control w-full max-w-xs">
             <label className="label"><span className="label-text">Email</span></label>
-            <input type="text" {...register("email")} placeholder="Email" className="input input-bordered w-full max-w-xs" />
+            <input
+              type="email" {...register("email", {
+                required: "Email Address is required"
+              })}
+              placeholder="Email" className="input input-bordered w-full max-w-xs" />
+            {errors.email && <p className="text-red-500 font-semibold mt-1">{errors.email?.message}</p>}
           </div>
           <div className="form-control w-full max-w-xs">
             <label className="label"><span className="label-text">Password</span></label>
-            <input type="password" {...register("password")} placeholder="Password" className="input input-bordered w-full max-w-xs" />
+            <input
+              type="password" {...register("password", {
+                required: "Password is required",
+                minLength: {value: 6, message: 'Password must be 6 characters or longer'}
+              })}
+              placeholder="Password" className="input input-bordered w-full max-w-xs" />
             <label className="label"><span className="label-text">Forget Password?</span></label>
+            {errors.password && <p className="text-red-500 font-semibold">{errors.password?.message}</p>}
           </div>
           <input className="btn btn-accent w-full" value="Login" type="submit" />
         </form>
